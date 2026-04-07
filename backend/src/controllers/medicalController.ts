d4743544ed26cef6f992733f49b9c97993cd45ae
Ga1ax9y <sasha20050803@gmail.com>
2026-04-07 17:26:55 +0300

    /medical updated

--- backend/src/controllers/medicalController.ts
+++ backend/src/controllers/medicalController.ts
@@ -1,6 +1,68 @@
 import type { NextFunction, Request, Response } from "express";
 import { prisma } from "../lib/prisma";
 import { AppError, commonErrorDict } from "../types/AppError";
+import type { MedicalHistoryWhereInput } from "../generated/prisma/models";
+import type { AuthRequest } from "../middlewares/authMiddleware";
+import { getPagination } from "../services/pagination";
+import { paginatedResponse } from "../services/paginatedResponse";
+
+const buildMedicalWhereClause = (query: any, playerId: number) => {
+    const where: MedicalHistoryWhereInput = {}
+
+    const { recoveryDate } = query
+
+    where.playerId = Number(playerId)
+
+    if (recoveryDate) {
+        where.recoveryDate = new Date(recoveryDate as string)
+    }
+
+    return where
+}
+
+export const getMedicalHistory = async (req: AuthRequest, res: Response, next: NextFunction) => {
+    try {
+        const {
+            sortBy = "recoveryDate",
+            order = "desc",
+            ...filters
+        } = req.query
+        const { id } = req.params
+        const { page, limit, skip } = getPagination(req.query);
+        const where: MedicalHistoryWhereInput = buildMedicalWhereClause(filters, Number(id))
+        const [medicalRecords, total] = await Promise.all([
+            prisma.medicalHistory.findMany({
+                where,
+                skip,
+                take: limit,
+                orderBy: {
+                    [sortBy as string]: order
+                }
+            }),
+            prisma.medicalHistory.count({ where })
+        ])
+        const transformedMedicalRecords = medicalRecords.map(medical => ({
+            id: medical.id,
+            playerId: medical.playerId,
+            injuryDate: medical.injuryDate,
+            recoveryDate: medical.recoveryDate,
+            diagnosis: medical.diagnosis,
+            status: medical.status,
+            createdAt: medical.createdAt,
+            updatedAt: medical.updatedAt
+
+        }));
+
+        res.json(paginatedResponse(transformedMedicalRecords, total, page, limit));
+    } catch (error: any) {
+        next(new AppError(
+            commonErrorDict.serverError.name,
+            commonErrorDict.serverError.httpCode,
+            error.message,
+            "Ошибка при получении медицинской истории игрока"
+        ));
+    }
+};
 
 export const markPlayerRecovered = async (req: Request, res: Response, next: NextFunction) => {
     try {
--- backend/src/routes/medicalRoutes.ts
+++ backend/src/routes/medicalRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import authenticateToken from "../middlewares/authMiddleware"
 import { checkRole } from "../middlewares/roleMiddleware";
-import { markPlayerRecovered } from "../controllers/medicalController";
+import { getMedicalHistory, markPlayerRecovered } from "../controllers/medicalController";
 
 const router = Router()
 
+router.get("/:id", authenticateToken, checkRole(['ADMIN', 'DOCTOR', 'COACH']), getMedicalHistory)
 router.patch("/:id/recover", authenticateToken, checkRole(['ADMIN', 'DOCTOR']), markPlayerRecovered)
 
 
